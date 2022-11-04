ee83cee7c8ce106233ec0c823cdc4e81b22d6bfa
epopov <mr.demix@yandex.ru>
2022-11-04 22:32:25 +0300

    Init commit

---
+++ src/main/java/com/epopov/App.java
@@ -0,0 +1,13 @@
+package com.epopov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
