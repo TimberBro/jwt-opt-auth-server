b2e48f5b5f1609b220ac3e161f52c71a0db94fb5
desktop-epopov\epopov <mr.demix@yandex.ru>
2023-01-14 18:13:39 +0300

    Added properties for mysql database and database schema

---
+++ auth-server/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `auth`.`user` (
+    `username` VARCHAR(45) NULL,
+    `password` TEXT NULL,
+    PRIMARY KEY (`username`));
+CREATE TABLE IF NOT EXISTS `auth`.`otp` (
+    `username` VARCHAR(45) NOT NULL,
+    `code` VARCHAR(45) NULL,
+    PRIMARY KEY (`username`));
