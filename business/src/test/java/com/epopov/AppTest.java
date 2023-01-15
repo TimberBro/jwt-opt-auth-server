b622ed52f1374312fc523815e2f9f2878d6d4da1
desktop-epopov\epopov <mr.demix@yandex.ru>
2023-01-15 22:00:49 +0300

    Fix tests

---
+++ business/src/test/java/com/epopov/AppTest.java
@@ -0,0 +1,15 @@
+package com.epopov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
